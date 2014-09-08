2aa8c4d414fd326157e55217d1347db7238696ff
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-09-08 09:46:59 +0200

    just in progress

--- Control/Admin/Form.php
+++ Control/Admin/Form.php
@@ -13,13 +13,87 @@ namespace phpManufaktur\ContactForm\Control\Admin;
 
 use Silex\Application;
 
-class Form extends Admin {
+class Form extends Admin
+{
 
+
+    protected function getContactForm($fields=array(), $data=array())
+    {
+        // create the form
+        $form = $this->app['form.factory']->createBuilder('form', null,
+            array('csrf_protection' => self::$config['form']['csrf']));
+
+        foreach (self::$config['form']['field']['list'] as $field) {
+            // checkbox to select the field
+            $form->add('field_'.$field, 'checkbox', array(
+                'data' => (isset($data[$field]['field']) || in_array($field, self::$config['form']['field']['must_have'])),
+                'required' => ($field === 'communication_email'),
+                'attr' => array('class' => 'field-select'),
+                'disabled' => ($field === 'communication_email')
+            ));
+            // checkbox to set the field as required
+            $form->add('required_'.$field, 'checkbox', array(
+                'data' => (isset($data[$field]['required']) || in_array($field, self::$config['form']['field']['required'])),
+                'required' => ($field === 'communication_email'),
+                'attr' => array('class' => 'field-require'),
+                'disabled' => ($field === 'communication_email')
+            ));
+            // checkbox to hide the field
+            $form->add('hidden_'.$field, 'checkbox', array(
+                'data' => isset($data[$field]['hidden']),
+                'required' => false,
+                'attr' => array('class' => 'field-hidden'),
+                'disabled' => !in_array($field, self::$config['form']['field']['hidden'])
+            ));
+        }
+
+        // add contact types
+        $form->add('contact_types', 'choice', array(
+            'choices' => array(
+                'PERSON' => 'Person',
+                'COMPANY' => 'Company',
+                'MIXED' => 'Mixed'
+            ),
+            'expanded' => false,
+            'empty_value' => false,
+            'data' => null
+        ));
+
+        // add contact categories
+        $categories = $this->app['contact']->getCategoryArrayForTwig();
+        $form->add('contact_categories', 'choice', array(
+            'choices' => $categories,
+            'expanded' => false,
+            'empty_value' => false,
+            'data' => null
+        ));
+
+        // add contact tags
+        $tags = $this->app['contact']->getTagArrayForTwig();
+        $form->add('contact_tags', 'choice', array(
+            'choices' => $tags,
+            'expanded' => true,
+            'multiple' => true,
+            'data' => null
+        ));
+
+        return $form->getForm();
+    }
 
     public function Controller(Application $app, $form_id)
     {
         $this->initialize($app);
 
-        return __METHOD__;
+        $form = $this->getContactForm();
+
+        return $this->app['twig']->render($this->app['utils']->getTemplateFile(
+            '@phpManufaktur/ContactForm/Template', 'admin/edit.form.twig'),
+            array(
+                'alert' => $this->getAlert(),
+                'config' => self::$config,
+                'usage' => self::$usage,
+                'toolbar' => $this->getToolbar('edit'),
+                'form' => $form->createView()
+            ));
     }
 }
--- Control/Configuration.php
+++ Control/Configuration.php
@@ -47,6 +47,84 @@ class Configuration
                 ),
                 'default' => 'about'
             ),
+            'form' => array(
+                'csrf' => true,
+                'field' => array(
+                    'hidden' => array(
+                        'contact_id',
+                        'contact_category',
+                        'contact_tag',
+                        'contact_type',
+                        'address_primary_id',
+                        'address_company_id',
+                        'address_delivery_id',
+                        'address_billing_id'
+                    ),
+                    'required' => array(
+                        'communication_email'
+                    ),
+                    'must_have' => array(
+                        'communication_email'
+                    ),
+                    'list' => array(
+                        'contact_type',
+                        'contact_category',
+                        'contact_tags',
+                        'contact_note',
+
+                        'company_name',
+                        'company_department',
+
+                        'person_gender',
+                        'person_title',
+                        'person_first_name',
+                        'person_last_name',
+                        'person_nick_name',
+                        'person_birthday',
+
+                        'communication_email',
+                        'communication_phone',
+                        'communication_cell',
+                        'communication_fax',
+                        'communication_url',
+
+                        'address_private_street',
+                        'address_private_zip',
+                        'address_private_city',
+                        'address_private_area',
+                        'address_private_state',
+                        'address_private_country_code',
+
+                        'address_business_street',
+                        'address_business_zip',
+                        'address_business_city',
+                        'address_business_area',
+                        'address_business_state',
+                        'address_business_country_code',
+
+                        'address_delivery_street',
+                        'address_delivery_zip',
+                        'address_delivery_city',
+                        'address_delivery_area',
+                        'address_delivery_state',
+                        'address_delivery_country_code',
+
+                        'address_delivery_street',
+                        'address_delivery_zip',
+                        'address_delivery_city',
+                        'address_delivery_area',
+                        'address_delivery_state',
+                        'address_delivery_country_code',
+
+                        'address_billing_street',
+                        'address_billing_zip',
+                        'address_billing_city',
+                        'address_billing_area',
+                        'address_billing_state',
+                        'address_billing_country_code',
+                    )
+                )
+            )
         );
     }
 
