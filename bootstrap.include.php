4a23de4788e1b83b8cfd753ebc227cafca89b09f
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-09-13 04:59:04 +0200

    just in progress

--- bootstrap.include.php
+++ bootstrap.include.php
@@ -4,7 +4,7 @@
  * ContactForm
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://kit2.phpmanufaktur.de/miniShop
+ * @link https://kit2.phpmanufaktur.de/ContactForm
  * @copyright 2014 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
